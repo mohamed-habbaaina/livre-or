3abdbd37107da929fd7ca34602c8cfa3e5f98889
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-12-05 12:56:43 +0100

    le header

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<header>
+    <a href="#"><img src="img/logo" alt="logo" class="logo">LOGO</a>
+    <nav>
+        <ul class="nav_bar">
+            <li><a href="index.php">Home</a></li>    
+            <li><a href="connexion.php">Se Connecter</a></li>    
+            <li><a href="inscription.php">Inscription</a></li>    
+            <li><a href="livre-or.php">Le Livre d'Or</a></li>    
+        </ul>    
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+            <div class="line4"></div>
+        </div>
+    </nav>
+</header>
