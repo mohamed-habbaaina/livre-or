0124c7402336265b7fb5210d542ee22565336672
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-12-05 12:52:47 +0100

    preparation du dossier livre d'or

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- MOHAMED HABBAAINA Le 05/12/2022
+
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Home</title>
+</head>
+<body>
+<?php include 'includes/header.php' ?>
+<header>
+    <a href="#"><img src="img/logo" alt="logo" class="logo">LOGO</a>
+    <nav>
+        <ul class="nav_bar">
+            <li><a href="index.php">Home</a></li>    
+            <li><a href="connexion.php">Se Connecter</a></li>    
+            <li><a href="inscription.php">Inscription</a></li>    
+            <li><a href="livre-or.php">Le Livre d'Or</a></li>    
+        </ul>    
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+            <div class="line4"></div>
+        </div>
+    </nav>
+</header>
+
+<footer>
+    <button class="btn_git"><a href="https://github.com/mohamed-habbaaina/livre-or">Lien GITHUB</a></button>
+</footer>
+<?php include 'includes/footer.php' ?>
+</body>
+</html>
