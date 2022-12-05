687ef1ecc25f92ee49aa073ffa00fc57e74cbfe9
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-12-05 13:35:56 +0100

    le dossier style css

--- index.php
+++ index.php
@@ -12,27 +12,9 @@
 </head>
 <body>
 <?php include 'includes/header.php' ?>
-<header>
-    <a href="#"><img src="img/logo" alt="logo" class="logo">LOGO</a>
-    <nav>
-        <ul class="nav_bar">
-            <li><a href="index.php">Home</a></li>    
-            <li><a href="connexion.php">Se Connecter</a></li>    
-            <li><a href="inscription.php">Inscription</a></li>    
-            <li><a href="livre-or.php">Le Livre d'Or</a></li>    
-        </ul>    
-        <div class="burger">
-            <div class="line1"></div>
-            <div class="line2"></div>
-            <div class="line3"></div>
-            <div class="line4"></div>
-        </div>
-    </nav>
-</header>
 
-<footer>
-    <button class="btn_git"><a href="https://github.com/mohamed-habbaaina/livre-or">Lien GITHUB</a></button>
-</footer>
+
+
 <?php include 'includes/footer.php' ?>
 </body>
 </html>
