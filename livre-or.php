26cd2cf71661b88aef69730a9f03850f8be352c4
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-12-09 16:59:32 +0100

    livre d'or, ajout de laffichage de input commentaires si l'lutilisateur es connecter

--- livre-or.php
+++ livre-or.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include 'includes/connect.php';
 // requette pour recupérer un tableau joiture id utilisateurs & id_utilisateur commentaire
 $requ_comm_all = $connection->query("SELECT login, commentaire, date FROM utilisateurs INNER JOIN commentaires ON utilisateurs.id = commentaires.id_utilisateur  ORDER BY date DESC;");
@@ -37,5 +38,48 @@ while ($comm_fetch = $requ_comm_all->fetch_assoc()){
     }?>
         </tbody>
      </table>
+<?php
+if (isset($_SESSION['login'])) { 
+    if (isset($_GET['submit'])){
+
+        //  connexion BD.
+        require 'includes/connect.php';
+    
+        // le commentaire.
+        $commentaire = htmlspecialchars(strip_tags(trim($_GET['commentaire'])));
+    
+        $compt_len = strlen($commentaire);
+        if ($compt_len > 6){
+    
+        //  recupirer la date du poste
+        $requ_inser = $connection->query("INSERT INTO `commentaires`(`commentaire`, `id_utilisateur`, `date`) VALUES ('$commentaire','$id',NOW());");
+        $mess_inser = 'Votre message est bien enregistré <a href="livre-or.php">"Livre d\'Or"</a> !';
+    } else {
+            $err_comm = 'Votre commentaire est trop court -Minimum 6 caractère!';
+    }
+    }
+    
+    
+    
+    ?>
+     <div class=commen_or>
+    <form action="#" type="get" class="form_comme">
+    <p class="mess_inser"><?php
+    if (isset($mess_inser)) {
+        echo $mess_inser;
+    }
+    if (isset($err_comm)) {
+        echo $err_comm;
+    } ?>
+
+
+    <label for="commentaire">Votre Commentaire</label>
+    <input type="textarea" name="commentaire" placeholder="Poster Votre Commentaire Ici">
+    <input type="submit" name="submit" value="Envoyer">
+
+
+
+    </div>
+<?php }?>
 </body>
 </html>
