9b36db6d2f645e4fdc2d5d783f6d523b175408f7
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-12-12 17:25:46 +0100

    connexion plesk

--- includes/connect.php
+++ includes/connect.php
@@ -7,9 +7,9 @@
 
         // Connexion plesk
         // $servername = 'localhost:3306';
-        // $username = 'modul_connexion';
-        // $password_b = 'Mmodul1234';
-        // $database = 'mohamed-habbaaina_moduleconnexion';
+        // $username = 'livre_or';
+        // $password_b = 'd920?kkG5';
+        // $database = 'mohamed-habbaaina_livreor';
         
         // Ce connecter a la base de données "utilisateurs"
         $connection = new mysqli($servername, $user_name, $password_b, $database) or die('Erreur');
